8761c68d2e97b745e109f0c49b4f692fe0e6c8bb
Light_Horse <tkdguq888@gmail.com>
2025-12-21 23:37:21 +0900

    db 수정

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+// 🔍 이 줄을 추가해서 브라우저 콘솔(F12)을 확인하세요!
+console.log("Supabase Key Check:", supabaseKey); 
+
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
